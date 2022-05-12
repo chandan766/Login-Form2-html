12c80ece126608cf7c61ec5c0e15a14bf606406c
chandan maurya <76623223+cr3992@users.noreply.github.com>
2022-05-13 01:23:04 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,6 @@
+function login_data(){
+    var u,p,a;
+    u =  document.getElementById("usrname").value;
+    p = document.getElementById("passw").value;
+    alert("Username: "+u+"\n"+"Password: "+p);
+}
